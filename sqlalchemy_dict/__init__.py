dd526ed4960943ae4b2c49130198b630b5a35bfe
meyt <pasd3000@gmail.com>
2017-09-26 18:23:57 +0330

    v0.1.1

--- sqlalchemy_dict/__init__.py
+++ sqlalchemy_dict/__init__.py
@@ -2,4 +2,4 @@ from .formatter import Formatter, DefaultFormatter
 from .base_model import BaseModel
 from .field import Field, relationship, composite
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
