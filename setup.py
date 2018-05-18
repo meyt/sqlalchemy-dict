0fbbfcbe69eba31709862bf9372a3a77fcc3dde9
meyt <pasd3000@gmail.com>
2018-05-18 16:15:56 +0430

    Add typing module as dependencies for python versions older than 3.5

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup, find_packages
 import os.path
 import re
+import sys
 
 package_name = 'sqlalchemy_dict'
+py_version = sys.version_info[:2]
 
 # reading package's version (same way sqlalchemy does)
 with open(os.path.join(os.path.dirname(__file__), package_name, '__init__.py')) as v_file:
@@ -11,6 +13,9 @@ with open(os.path.join(os.path.dirname(__file__), package_name, '__init__.py')) 
 dependencies = [
   'sqlalchemy'
 ]
+
+if py_version < (3, 5):
+    dependencies.append('typing')
 
 setup(
     name=package_name,
