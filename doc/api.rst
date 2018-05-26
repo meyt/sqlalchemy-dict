2aaf081aaa4c0aa7d5ea8575e060be9ddef04ad0
meyt <pasd3000@gmail.com>
2018-05-26 20:34:00 +0430

    Add formatter docstring

--- doc/api.rst
+++ doc/api.rst
@@ -26,6 +26,7 @@ Formatter
 ^^^^^^^^^
 
 .. autoclass:: Formatter
+    :members:
 
 
 DefaultFormatter
--- sqlalchemy_dict/formatter.py
+++ sqlalchemy_dict/formatter.py
@@ -1,37 +1,80 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, time
 
 from sqlalchemy_dict.utils import format_iso_datetime, format_iso_time, to_camel_case
 from sqlalchemy_dict.constants import ISO_DATETIME_FORMAT, ISO_DATE_FORMAT, ISO_DATETIME_PATTERN, ISO_TIME_FORMAT
 
 
 class Formatter:
+    """ Model formatter abstract class """
 
     @classmethod
     def export_key(cls, key):
+        """
+        Export dictionary key
+
+        :param key: Model field name
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def export_datetime(cls, value):
+    def export_datetime(cls, value: datetime):
+        """
+        Export python datetime
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def export_date(cls, value):
+    def export_date(cls, value: date):
+        """
+        Export python date
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def export_time(cls, value):
+    def export_time(cls, value: time):
+        """
+        Export python time
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def import_datetime(cls, value):
+    def import_datetime(cls, value: [str, int]) -> datetime:
+        """
+        Import datetime field
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def import_date(cls, value):
+    def import_date(cls, value: [str, int]) -> date:
+        """
+        Import date
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
     @classmethod
-    def import_time(cls, value):
+    def import_time(cls, value: [str, int]) -> time:
+        """
+        Import time
+
+        :param value:
+        :return:
+        """
         raise NotImplemented  # pragma: no cover
 
 
